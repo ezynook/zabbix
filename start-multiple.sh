7710fcc47d6c5d5e9486a5cebb3bfde19e7bd397
pasit.dev <90300159+ezynook@users.noreply.github.com>
2023-05-29 15:06:22 +0700

    Create start-multiple.sh

---
+++ start-multiple.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Zabbix Service Starting..."
+docker-compose -f docker-compose.multiple.yml up -d > /dev/null
+docker exec -it zabbix /bin/bash -c "sed -i 's/DBHost=localhost/DBHost=zabbixdb/g' /etc/zabbix/zabbix_server.conf" > /dev/null
+docker restart zabbix > /dev/null
+echo "Zabbix Service Starting..."
